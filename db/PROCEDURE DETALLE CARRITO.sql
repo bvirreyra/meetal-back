d9e8aa075da1279b579199e88ddfac64b1fa1961
Boris Virreyra <bvirreyra34@gmail.com>
2024-11-20 19:18:50 -0400

    agrega-nuevo-procedure

--- db/PROCEDURE DETALLE CARRITO.sql
+++ db/PROCEDURE DETALLE CARRITO.sql
@@ -1,4 +1,5 @@
-CREATE PROCEDURE meetal
+--- PROCEDIMIENTO PARA VER EL CARRITO DEL CLIENTE
+CREATE PROCEDURE carrito_cliente
 AS
 	SELECT cl.nombre, (cl.correo) AS correo,(c.fecha_creacion) AS fecha_creación, (p.nombre) AS producto, (p.descripcion) AS descripcion, (dc.precio_unidad) AS precio_unidad, (dc.cantidad) AS cantidad, (dc.precio_unidad*dc.cantidad) AS subtotal 
 	FROM detalle_carrito dc
@@ -7,4 +8,15 @@ AS
 		join producto p ON p.producto_id=dc.producto_id
 	WHERE c.cliente_id=1
 
-EXEC meetal
+EXEC carrito_cliente
+
+--- PROCEDIMIENTO PARA EL ESTADO DEL CARRITO DEL CLIENTE
+CREATE PROCEDURE estado_carrito
+AS
+SELECT cl.nombre, cl.correo, c.fecha_creacion, ic.nombre_estado
+FROM cliente cl
+	join carrito c ON cl.cliente_id=c.cliente_id
+	join identificador_carrito ic ON c.estado=ic.estado_id
+WHERE cl.cliente_id=1
+
+EXEC estado_carrito
